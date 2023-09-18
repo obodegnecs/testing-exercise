f18c19d870cdd41e1d644b52a54201094dccea84
Csenge Dobo <csenge_dobo@epam.com>
2023-09-18 18:28:36 +0200

    Create sorting functions and tests

--- app/task/TaskList.php
+++ app/task/TaskList.php
@@ -5,27 +5,59 @@ use InvalidArgumentException;
 
 class TaskList
 {
-    private array $tasks;
+    private array $_tasks;
 
-    public function add(Task $task): void {
-        $this->tasks[] = $task;
+    public function add(Task $task): void
+    {
+        $this->_tasks[] = $task;
     }
 
-    public function get(int $index): ?Task {
+    public function get(int $index): ?Task
+    {
         if ($index < 0) {
             throw new InvalidArgumentException;
         }
          return $this->tasks[$index] ?? null;
     }
 
-    public function getById(int $id): ?Task {
-        foreach ($this->tasks as $task) {
-            if ($task->id === $id)
-            return $task;
+    public function getById(int $id): ?Task
+    {
+        foreach ($this->_tasks as $task) {
+            if ($task->id === $id) {
+                return $task;
+            }
         }
 
         return null;
     }
 
+    public function sortByName(): void
+    {
+        $this->sortBy("name");
+    }
 
+    public function sortById(): void
+    {
+        $this->sortBy("id");
+    }
+
+    public function sortBy(string $column): void
+    {
+        switch ($column){
+        case "name":
+            usort(
+                $this->_tasks,
+                fn($task1, $task2) => strcmp($task1->name, $task2->name)
+            );
+            break;
+        case "id":
+            usort(
+                $this->_tasks,
+                fn($task1, $task2) => $task1->id - $task2->id
+            );
+            break;
+        default: 
+            throw new InvalidArgumentException;
+        }
+    }
 }
--- app/tests/TaskListTest.php
+++ app/tests/TaskListTest.php
@@ -6,13 +6,15 @@ use PHPUnit\Framework\TestCase;
 
 class TaskListTest extends TestCase
 {
-    public function testNullReturnedFromGetWhenEmpty() {
+    public function testNullReturnedFromGetWhenEmpty()
+    {
         $task_list = new TaskList();
 
         $this->assertNull($task_list->get(0));
     }
 
-    public function testSameTaskAddedAndReturnedFromTaskList() {
+    public function testSameTaskAddedAndReturnedFromTaskList()
+    {
         $new_task = new Task();
         $task_list = new TaskList();
 
@@ -21,7 +23,8 @@ class TaskListTest extends TestCase
         $this->assertSame($new_task, $task_list->get(0));
     }
 
-    public function testSameThreeTaskAddedAndReturnedFromTaskList() {
+    public function testSameThreeTaskAddedAndReturnedFromTaskList()
+    {
         $new_task1 = new Task();
         $new_task2 = new Task();
         $new_task3 = new Task();
@@ -36,7 +39,8 @@ class TaskListTest extends TestCase
         $this->assertSame($new_task3, $task_list->get(2));
     }
 
-    public function testNullReturnedFromGetByIdWhenIdNotInTaskList() {
+    public function testNullReturnedFromGetByIdWhenIdNotInTaskList()
+    {
         $new_task1 = new Task();
         $new_task1->id = 0;
 
@@ -51,7 +55,8 @@ class TaskListTest extends TestCase
         $this->assertNull($task_list->getById(2));
     }
 
-    public function testSameThreeTaskAddedAndReturnedByIdFromTaskList() {
+    public function testSameThreeTaskAddedAndReturnedByIdFromTaskList()
+    {
         $new_task1 = new Task();
         $new_task1->id = 0;
 
@@ -67,11 +72,106 @@ class TaskListTest extends TestCase
         $this->assertSame($new_task2, $task_list->getById(1));
     }
 
-    public function testExceptionThrownFromGetWhenIndexIsNegative() {
+    public function testExceptionThrownFromGetWhenIndexIsNegative()
+    {
         $this->expectException(InvalidArgumentException::class);
 
         $task_list = new TaskList();
         $task_list->get(-1);
     }
 
+    public function testSortByNameAscendingOrder()
+    {
+        $new_task1 = new Task();
+        $new_task1->name = "Ironing the clothes"; // 1. in order
+        $new_task1->id = 22;
+
+        $new_task2 = new Task();
+        $new_task2->name = "Loading the washing machine"; // 2. in order
+        $new_task2->id = 2;
+
+        $new_task3 = new Task();
+        $new_task3->name = "Mopping the floor"; // 3. in order
+        $new_task3->id = 222;
+
+        $task_list = new TaskList();
+
+        $task_list->add($new_task3);
+        $task_list->add($new_task2);
+        $task_list->add($new_task1);
+
+        $task_list->sortByName();
+
+        $this->assertSame($new_task1, $task_list->get(0));
+        $this->assertSame($new_task2, $task_list->get(1));
+        $this->assertSame($new_task3, $task_list->get(2));
+    }
+
+    public function testSortByIdAscendingOrder()
+    {
+        $new_task1 = new Task();
+        $new_task1->name = "Write test";
+        $new_task1->id = 4; // 1. in order
+
+        $new_task2 = new Task();
+        $new_task2->name = "Run test";
+        $new_task2->id = 17; // 2. in order
+
+        $new_task3 = new Task();
+        $new_task3->name = "Write code";
+        $new_task3->id = 34; // 3. in order
+
+        $task_list = new TaskList();
+
+        $task_list->add($new_task2);
+        $task_list->add($new_task3);
+        $task_list->add($new_task1);
+
+        $task_list->sortById();
+
+        $this->assertSame($new_task1, $task_list->get(0));
+        $this->assertSame($new_task2, $task_list->get(1));
+        $this->assertSame($new_task3, $task_list->get(2));
+    }
+
+    public function testExceptionThrownFromSortByWhenColumnInvalid()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $task_list = new TaskList();
+        $task_list->sortBy("category");
+    }
+
+    public function testSortByWhenColumnIsValid()
+    {
+        $new_task1 = new Task();
+        $new_task1->name = "Write test"; // 3. NAME in order
+        $new_task1->id = 3; // 1. ID in order
+
+        $new_task2 = new Task();
+        $new_task2->name = "Run test"; // 1. NAME in order
+        $new_task2->id = 16; // 2. ID in order
+
+        $new_task3 = new Task();
+        $new_task3->name = "Write code";// 2. NAME in order
+        $new_task3->id = 33; // 3. ID in order
+        
+        $task_list = new TaskList();
+
+        $task_list->add($new_task3);
+        $task_list->add($new_task2);
+        $task_list->add($new_task1);
+
+        $task_list->sortBy("id");
+
+        $this->assertSame($new_task1, $task_list->get(0));
+        $this->assertSame($new_task2, $task_list->get(1));
+        $this->assertSame($new_task3, $task_list->get(2));
+
+        $task_list->sortBy("name");
+
+        $this->assertSame($new_task2, $task_list->get(0));
+        $this->assertSame($new_task3, $task_list->get(1));
+        $this->assertSame($new_task1, $task_list->get(2));
+    }
 }
